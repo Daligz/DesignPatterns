e5d1ad08b2c9ee64556755b51712bdb158445f48
Daligz <oscardali370@gmail.com>
2022-05-18 11:45:02 -0500

    Minor code style fix

--- src/main/java/me/upp/daligz/designpatterns/structural/decorator/examples/pizzas/PizzaMaker.java
+++ src/main/java/me/upp/daligz/designpatterns/structural/decorator/examples/pizzas/PizzaMaker.java
@@ -4,7 +4,8 @@ public class PizzaMaker {
 
     public static void main(String[] args) {
         Pizza pizza = new Mozzarella(
-                new Cheese(new Letuce(new Meat(new Pepperoni(new Pineapple(new Tomato(new TomatoSauce(new PlainPizza()))))))));
+                new Cheese(new Letuce(new Meat(new Pepperoni(new Pineapple(new Tomato(new TomatoSauce(new PlainPizza())))))))
+        );
         System.out.println("Ingredients: " + pizza.getDescription());
         System.out.println("Total Price: " + pizza.getPrice());
     }
